bf681513d2cd5c48673231b6730c64f9cb260eca
LucasCarmagnola <108589707+LucasCarmagnola@users.noreply.github.com>
2024-08-22 18:45:28 -0300

    first commit

---
+++ funciones.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoLink = void 0;
+function GoLink() {
+    window.location.href = "https://www.instagram.com/sofinuske/";
+}
+exports.GoLink = GoLink;
---
+++ funciones.ts
@@ -0,0 +1,4 @@
+export function GoLink(){
+    window.location.href = "https://www.instagram.com/sofinuske/";
+}
+
